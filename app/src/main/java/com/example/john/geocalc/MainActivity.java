25be5dd2a8b3734e89a5f4df09ffc040e4749b84
jdegrandchamp89 <degranjo@mail.gvsu.edu>
2017-05-22 18:50:42 -0400

    GeoCalc for Android
    Geocalculator for Android project

---
+++ app/src/main/java/com/example/john/geocalc/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.john.geocalc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.renderscript.Double2;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.location.Location;
+import android.widget.TextView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String distanceUnit = "Kilometers";
+    String bearingUnit = "Degrees";
+    String lat1;
+    String lat2;
+    String long1;
+    String long2;
+    boolean keyboard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText latitude1 = (EditText) findViewById(R.id.latitude1);
+        EditText latitude2 = (EditText) findViewById(R.id.latitude2);
+        EditText longitude1= (EditText) findViewById(R.id.longitude1);
+        EditText longitude2 = (EditText) findViewById(R.id.longitude2);
+        Button calculate = (Button) findViewById(R.id.calculate);
+        Button clear = (Button) findViewById(R.id.clear);
+        TextView distanceValue = (TextView) findViewById(R.id.distanceValue);
+        TextView bearingValue = (TextView) findViewById(R.id.bearingValue);
+
+        Intent payload = getIntent();
+        if (payload.hasExtra("distanceUnit")) {
+            distanceUnit = payload.getStringExtra("distanceUnit");
+        }
+        if (payload.hasExtra("bearingUnit")) {
+            bearingUnit = payload.getStringExtra("bearingUnit");
+        }
+        if (payload.hasExtra("lat1")) {
+            lat1 = payload.getStringExtra("lat1");
+            latitude1.setText(lat1);
+        }
+        if (payload.hasExtra("lat2")) {
+            lat2 = payload.getStringExtra("lat2");
+            latitude2.setText(lat2);
+        }
+        if (payload.hasExtra("long1")) {
+            long1 = payload.getStringExtra("long1");
+            longitude1.setText(long1);
+        }
+        if (payload.hasExtra("long2")) {
+            long2 = payload.getStringExtra("long2");
+            longitude2.setText(long2);
+
+            // if these values already exist use them to make calculation
+            if(lat1.length() != 0 && lat2.length() != 0 && long1.length() != 0 && long2.length() != 0){
+                Location location1 = new Location("");
+                location1.setLatitude(Double.parseDouble(lat1));
+                location1.setLongitude(Double.parseDouble(long1));
+
+                Location location2 = new Location("");
+                location2.setLatitude(Double.parseDouble(lat2));
+                location2.setLongitude(Double.parseDouble(long2));
+
+                Float distance2 = location1.distanceTo(location2);
+                distance2 *= Float.parseFloat("0.001");
+                if(distanceUnit.contentEquals("Miles")){
+                    distance2 *= Float.parseFloat("0.621371");
+                }
+
+                Float bearing2 = location1.bearingTo(location2);
+                if(bearingUnit.contentEquals("Mils")){
+                    bearing2 *= Float.parseFloat("17.777777777778");
+                }
+
+                distanceValue.setText(String.format("%.2f%n", distance2) + distanceUnit);
+                bearingValue.setText(String.format("%.2f%n", bearing2) + bearingUnit);
+            }
+
+        }
+
+        calculate.setOnClickListener(v -> {
+            lat1 = latitude1.getText().toString();
+            lat2 = latitude2.getText().toString();
+            long1 = longitude1.getText().toString();
+            long2 = longitude2.getText().toString();
+
+            if(lat1.length() != 0 && lat2.length() != 0 && long1.length() != 0 && long2.length() != 0){
+                Location loc1 = new Location("");
+                loc1.setLatitude(Double.parseDouble(lat1));
+                loc1.setLongitude(Double.parseDouble(long1));
+
+                Location loc2 = new Location("");
+                loc2.setLatitude(Double.parseDouble(lat2));
+                loc2.setLongitude(Double.parseDouble(long2));
+
+                Float distance = loc1.distanceTo(loc2);
+                distance *= Float.parseFloat("0.001");
+                if(distanceUnit.contentEquals("Miles")){
+                    distance *= Float.parseFloat("0.621371");
+                }
+
+                Float bearing = loc1.bearingTo(loc2);
+                if(bearingUnit.contentEquals("Mils")){
+                    bearing *= Float.parseFloat("17.777777777778");
+                }
+
+                distanceValue.setText(String.format("%.2f%n", distance) + distanceUnit);
+                bearingValue.setText(String.format("%.2f%n", bearing) + bearingUnit);
+            }
+
+            hideKeyboard();
+
+
+
+        });
+
+        clear.setOnClickListener(v -> {
+            latitude1.setText(null);
+            latitude2.setText(null);
+            longitude1.setText(null);
+            longitude2.setText(null);
+            distanceValue.setText(null);
+            bearingValue.setText(null);
+
+            hideKeyboard();
+        });
+
+    }
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if(item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            intent.putExtra("distanceUnit", distanceUnit);
+            intent.putExtra("bearingUnit", bearingUnit);
+            intent.putExtra("lat1", lat1);
+            intent.putExtra("lat2", lat2);
+            intent.putExtra("long1", long1);
+            intent.putExtra("long2", long2);
+
+            startActivity(intent);
+            //finish();
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/example/john/geocalc/SettingsActivity.java
@@ -0,0 +1,114 @@
+package com.example.john.geocalc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    String distanceString;
+    String bearingString;
+    String lat1;
+    String lat2;
+    String long1;
+    String long2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner distanceSpinner = (Spinner) findViewById(R.id.distanceUnit);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> distanceAdapter = ArrayAdapter.createFromResource(this,
+                R.array.distance_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        distanceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        distanceSpinner.setAdapter(distanceAdapter);
+
+        Spinner bearingSpinner = (Spinner) findViewById(R.id.bearingUnit);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> bearingAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bearing_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        bearingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        bearingSpinner.setAdapter(bearingAdapter);
+
+        Intent payload = getIntent();
+        if (payload.hasExtra("distanceUnit")) {
+            distanceString = payload.getStringExtra("distanceUnit");
+            if(distanceString.contentEquals("Kilometers")){
+                distanceSpinner.setSelection(0);
+            }
+            else if(distanceString.contentEquals("Miles")){
+                distanceSpinner.setSelection(1);
+            }
+        }
+
+        if (payload.hasExtra("bearingUnit")) {
+            bearingString = payload.getStringExtra("bearingUnit");
+            if(bearingString.contentEquals("Degrees")){
+                bearingSpinner.setSelection(0);
+            }
+            else if(bearingString.contentEquals("Mils")){
+                bearingSpinner.setSelection(1);
+            }
+        }
+
+        if (payload.hasExtra("lat1")) {
+            lat1 = payload.getStringExtra("lat1");
+        }
+        if (payload.hasExtra("lat2")) {
+            lat2 = payload.getStringExtra("lat2");
+        }
+        if (payload.hasExtra("long1")) {
+            long1 = payload.getStringExtra("long1");
+        }
+        if (payload.hasExtra("long2")) {
+            long2 = payload.getStringExtra("long2");
+        }
+
+
+        distanceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position,long id) {
+                distanceString =(String)parent.getSelectedItem();
+
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        bearingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position,long id) {
+                bearingString =(String)parent.getSelectedItem();
+
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        FloatingActionButton save = (FloatingActionButton) findViewById(R.id.saveButton);
+        save.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            intent.putExtra("distanceUnit", distanceString);
+            intent.putExtra("bearingUnit", bearingString);
+            intent.putExtra("lat1", lat1);
+            intent.putExtra("lat2", lat2);
+            intent.putExtra("long1", long1);
+            intent.putExtra("long2", long2);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+}
